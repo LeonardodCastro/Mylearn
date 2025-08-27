4443b7555f9285551a5ec9ceba0e1ea486d0f5d8
LeonardodCastro <dev.leonardocastro@gmail.com>
2025-08-27 16:16:24 -0300

    feat: project structure

---
+++ src/main/java/com/leonardodcastro/mylearn/MylearnApplication.java
@@ -0,0 +1,13 @@
+package com.leonardodcastro.mylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylearnApplication.class, args);
+	}
+
+}
